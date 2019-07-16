908ec447b735867c15b4c84dcad902d5d89c99ad
l z <2416305049@qq.com>
2019-07-17 00:05:14 +0800

    init

---
+++ src/components/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>home</h1>
+    <router-link to="/list">List</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/list/List.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    列表页
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
